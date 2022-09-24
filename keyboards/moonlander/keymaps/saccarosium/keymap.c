6bd5ca19a2bf2884c500cb7c4e9a1ac22a175cfa
saccarosium <github.e41mv@aleeas.com>
2022-09-24 18:46:09 +0200

    Adding arrow-keys

--- keyboards/moonlander/keymaps/saccarosium/keymap.c
+++ keyboards/moonlander/keymaps/saccarosium/keymap.c
@@ -23,6 +23,7 @@ enum layers {
     BAS,  // default layer
     HIG, // symbols layer
     LOW,  // extended layer
+    FUN, // fuctions
 };
 
 enum custom_keycodes {
@@ -34,30 +35,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BAS] = LAYOUT_moonlander(
         _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
         KC_TAB,  KC_QUOTE,KC_COMM, KC_DOT,  KC_P,    KC_Y,    _______,           _______, KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
-        KC_ESC,  KC_A,    KC_O,    ALT_T(KC_E),GUI_T(KC_U), KC_I,_______,        _______, KC_D,    GUI_T(KC_H),RALT_T(KC_T),  KC_N,    KC_S,    KC_MINS,
+        KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    _______,           _______, KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
         KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                                KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
-        _______, _______, _______, _______, _______,          _______,           _______,          _______, _______, _______, _______, _______,
-                                    KC_SPC, LT(HIG,KC_ENTER), _______,           _______, TT(LOW), CTL_T(KC_BSPC)
+        _______, _______, _______, KC_UP,   KC_DOWN,          KC_LGUI,           KC_LGUI,          KC_LEFT, KC_RGHT, _______, _______, _______,
+                                    KC_SPC, LT(LOW,KC_ENTER), KC_LALT,           KC_RALT, LT(HIG,KC_ENTER), CTL_T(KC_BSPC)
     ),
 
     [HIG] = LAYOUT_moonlander(
         _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,           _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        _______, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______,           _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,
-        _______, KC_F1,   KC_VOLD, KC_MUTE, KC_VOLU, _______,                             _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_F12,  _______,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,           _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+        _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______,           _______, _______, KC_EQL,  KC_BSLS, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,          _______,           _______,          _______, _______, _______, _______, _______,
                                             _______, _______, _______,           _______, _______, _______
     ),
 
     [LOW] = LAYOUT_moonlander(
         _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_EXLM, KC_RCBR, KC_RBRC, KC_RPRN, KC_GRV,  _______,           _______, KC_PERC, KC_TILD, KC_PIPE, KC_HASH, KC_DLR,  _______,
-        _______, KC_AT,   KC_LCBR, KC_LBRC, KC_LPRN, KC_AMPR, _______,           _______, KC_CIRC, KC_EQL,  KC_PLUS, KC_ASTR, KC_BSLS, _______,
-        _______, _______, _______, KC_DOWN, KC_UP,   _______,                             _______, KC_RGHT, KC_LEFT, _______, _______, _______,
+        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  _______,          _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
+        _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______,           _______, _______, KC_LPRN, KC_RPRN, _______, _______, KC_PIPE,
+        _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,          _______,           _______,          _______, _______, _______, _______, _______,
                                             _______, _______, _______,           _______, _______, _______
     ),
 
+    [FUN] = LAYOUT_moonlander(
+        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
+        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  _______,          _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
+        _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______,           _______, _______, KC_LPRN, KC_RPRN, _______, _______, KC_PIPE,
+        _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______,          _______,           _______,          _______, _______, _______, _______, _______,
+                                            _______, _______, _______,           _______, _______, _______
+    ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
